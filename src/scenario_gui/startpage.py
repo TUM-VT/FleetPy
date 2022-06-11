a9e671e8f6f12f6dee816929205b1effdd35fbe3
akshayp <akshay.pimpalkar@tum.de>
2022-06-11 15:15:50 +0200

    adding save to csv func

--- src/scenario_gui/startpage.py
+++ src/scenario_gui/startpage.py
@@ -14,7 +14,8 @@ class StartPage(tk.Frame):
         self.id = controller.mandatory_module
         row_count=1
         col_count=1
-        # page_heading = tk.Label(self, text = "Welcome Page \n" + controller.mandatory_module.get(), font=controller.titlefont)
+        list_of_selected_modules = []
+    
         page_heading = tk.Label(self, text = "Start Page", font=controller.titlefont).grid(row=0,column=0, columnspan=4,padx=20, pady=10)
 
         #Feature 1
@@ -24,30 +25,39 @@ class StartPage(tk.Frame):
         man_module_dict, op_module_dict = controller.sc.get_current_mandatory_and_optional_modules()
 
         for module_name, module_specifications in man_module_dict.items():
-
-            module = tk.StringVar()
-            module.set(module_name) 
-            module_view = tk.Label(self, text=module.get(), font=controller.normalfont).grid(row=row_count,column=col_count)
+            if module_specifications.type == "int":
+                module = tk.IntVar()
+                module.set(controller.default_int)
+            else:
+                module = tk.StringVar()
+                module.set(module_name) 
+            module_view = tk.Label(self, text=module_name, font=controller.normalfont).grid(row=row_count,column=col_count)
             sel_module = tk.OptionMenu(self,module, *module_specifications.options)
             sel_module.config(width=30)
             sel_module.grid(row=row_count, column=(col_count+1))  
             col_count = 2
-            row_count = row_count+1                     
+            row_count = row_count+1
+            list_of_selected_modules.append([module_name, module])                   
 
         #Feature 2
         optional_module = tk.Label(self, text=controller.optional_modules.get(), font=controller.normalfont).grid(row=row_count,column=1)
         row_count = row_count + 1
-
+        
         for module_name, module_specifications in op_module_dict.items():
 
-            module = tk.StringVar()
-            module.set(module_name) 
-            module_view = tk.Label(self, text=module.get(), font=controller.normalfont).grid(row=row_count,column=col_count)
+            if module_specifications.type == "int":
+                module = tk.IntVar()
+                module.set(controller.default_int)
+            else:
+                module = tk.StringVar()
+                module.set(controller.default_text) 
+            module_view = tk.Label(self, text=module_name, font=controller.normalfont).grid(row=row_count,column=col_count)
             sel_module = tk.OptionMenu(self,module, *module_specifications.options)
             sel_module.config(width=30)
             sel_module.grid(row=row_count, column=(col_count+1))
             col_count = 2
-            row_count = row_count+1            
+            row_count = row_count+1
+            list_of_selected_modules.append([module_name, module])          
 
-        back_button = tk.Button(self, text = "Next Page",
-                                command= lambda: controller.up_frame('PageOne')).grid(row=30, column=0, columnspan=4, padx=20, pady=10)
+        save_and_next_page = tk.Button(self, text = "Save and Next",
+                                command= lambda: controller.store_input_args('PageOne', list_of_selected_modules)).grid(row=30, column=0, columnspan=4, padx=20, pady=10)
