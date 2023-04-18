b6b6e6fb25c25b02c16526c0c91f905edfe951bf
Roman Engelhardt <roman.engelhardt@tum.de>
2023-04-18 16:12:35 +0200

    bugfix AM repo (small)

--- src/fleetctrl/repositioning/AlonsoMoraRepositioning.py
+++ src/fleetctrl/repositioning/AlonsoMoraRepositioning.py
@@ -72,9 +72,10 @@ class AlonsoMoraRepositioning(RepositioningBase):
         # possible repostionings (only idle vehicles)    
         vid_to_origin_to_tt = {}    # idle vehicle id -> possible origin of repo target -> travel time (to be minimized)
         origin_to_counts = Counter(self._rejected_customer_origins_since_last_step)   # origin -> number of occurances
-        for rej_o, number_rej in origin_to_counts.items():
-            zone_id = self.zone_system.get_zone_from_node(rej_o)
-            self.record_df.loc[(sim_time, zone_id, sim_time, sim_time + self.fleetctrl.repo_time_step), "tot_fc_supply"] = number_rej
+        if self.zone_system is not None:
+            for rej_o, number_rej in origin_to_counts.items():
+                zone_id = self.zone_system.get_zone_from_node(rej_o)
+                self.record_df.loc[(sim_time, zone_id, sim_time, sim_time + self.fleetctrl.repo_time_step), "tot_fc_supply"] = number_rej
         for veh in self.fleetctrl.sim_vehicles:
             if len(self.fleetctrl.veh_plans[veh.vid].list_plan_stops) == 0:
                 vid_to_origin_to_tt[veh.vid] = {}
