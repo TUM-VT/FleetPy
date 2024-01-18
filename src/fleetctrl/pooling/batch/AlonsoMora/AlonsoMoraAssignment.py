ffe0a5f85632e2758cf47ba95705379aba5178ab
Roman Engelhardt <roman.engelhardt@tum.de>
2024-01-18 11:48:11 +0100

    bugfix typechecking

--- src/fleetctrl/pooling/batch/AlonsoMora/AlonsoMoraAssignment.py
+++ src/fleetctrl/pooling/batch/AlonsoMora/AlonsoMoraAssignment.py
@@ -71,7 +71,10 @@ class AlonsoMoraAssignment(BatchAssignmentAlgorithmBase):
         self.max_rv_connections : int = operator_attributes.get(G_RA_MAX_VR, None)
         self.max_tour_per_v2rb : int = operator_attributes.get(G_RA_MAX_TOUR, None)
         if self.max_tour_per_v2rb is not None and type(self.max_tour_per_v2rb) != int:
-            self.max_tour_per_v2rb = int(self.max_tour_per_v2rb)
+            if np.isnan(self.max_tour_per_v2rb):
+                self.max_tour_per_v2rb = None
+            else:
+                self.max_tour_per_v2rb = int(self.max_tour_per_v2rb)
         applied_heuristics = operator_attributes.get(G_RA_HEU, None)
         
         self.applied_heuristics = {}
