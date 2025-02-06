ac224bc224e364724622fccd6b463b93fe176481
hoda_hamdy <hoda.hamdy@tum.de>
2025-02-06 14:45:08 +0100

    edit graph and node features

--- src/fleetctrl/pooling/batch/AlonsoMora/GNNAlonsoMoraAssignment.py
+++ src/fleetctrl/pooling/batch/AlonsoMora/GNNAlonsoMoraAssignment.py
@@ -1,11 +1,16 @@
 import pickle
 import os
-from typing import Dict, List, Callable, Tuple
+from typing import Dict, List, Callable, Tuple, Any
+from collections import defaultdict
 
 from src.fleetctrl.FleetControlBase import FleetControlBase
 from src.fleetctrl.pooling.batch.AlonsoMora.AlonsoMoraAssignment import AlonsoMoraAssignment
-from src.misc.globals import G_DIR_TRAIN, G_DIR_OUTPUT, G_TRAIN_RR_FILE, G_TRAIN_RV_FILE, G_TRAIN_ASSIGNMENTS_FILE, \
-    G_TRAIN_INIT_ASSIGNMENTS_FILE, G_TRAIN_FEATURE_TRAVEL_TIME, G_TRAIN_NODE_FEATURES_FILE
+from src.misc.globals import G_DIR_TRAIN, G_DIR_OUTPUT, G_TRAIN_RR_FILE, G_TRAIN_VR_FILE, G_TRAIN_ASSIGNMENTS_FILE, \
+    G_TRAIN_INIT_ASSIGNMENTS_FILE, G_TRAIN_FEATURE_TRAVEL_TIME, G_TRAIN_REQ_FEATURES_FILE, \
+    G_TRAIN_VEH_FEATURES_FILE, G_TRAIN_FEATURE_TRAVEL_DIST, G_TRAIN_FEATURE_TRAVEL_COST, G_TRAIN_FEATURE_NODE_TYPE, \
+    G_TRAIN_FEATURE_O_POS, G_TRAIN_FEATURE_D_POS, G_TRAIN_FEATURE_TW_PE, G_TRAIN_FEATURE_TW_PL, \
+    G_TRAIN_FEATURE_DIRECT_TT, G_TRAIN_FEATURE_DIRECT_TD, G_TRAIN_FEATURE_MAX_TRIP_TIME, G_TRAIN_FEATURE_STATUS, \
+    G_TRAIN_FEATURE_SOC, G_TRAIN_FEATURE_TYPE, G_TRAIN_FEATURE_V_POS
 from src.routing.NetworkBase import NetworkBase
 from src.simulation.Legs import VehicleRouteLeg
 from src.simulation.Vehicles import SimulationVehicle
@@ -26,74 +31,75 @@ class GNNAlonsoMoraAssignment(AlonsoMoraAssignment):
                                         new_travel_times: bool = False, build_from_scratch: bool = False):
         super().compute_new_vehicle_assignments(sim_time, vid_to_list_passed_VRLs, veh_objs_to_build, new_travel_times,
                                                 build_from_scratch)
-
         self.write_train_data(sim_time)
-        # TODO explore attributes for features
         # TODO save top 10 optimized assignments
 
-    def write_train_data(self, sim_time):
+    def write_train_data(self, sim_time: int):
         dir_path = os.path.join(self.train_data_path, str(sim_time))
         os.makedirs(dir_path, exist_ok=True)
         train_data = self.get_train_data()
-        for name, data in train_data:
+        for name, data in train_data.items():
             path = os.path.join(dir_path, f'{name}.pkl')
             self.write_pickle(path, data)
 
-    def get_train_data(self) -> List[Tuple[int, dict]]:
-        train_data = [(G_TRAIN_RR_FILE, self.get_rr_graph_with_features()),
-                      (G_TRAIN_RV_FILE, self.get_r2v_graph_with_features()),
-                      (G_TRAIN_ASSIGNMENTS_FILE, self.optimisation_solutions),
-                      (G_TRAIN_INIT_ASSIGNMENTS_FILE, self.current_assignments),
-                      (G_TRAIN_NODE_FEATURES_FILE, self.get_node_features())]
+    def get_train_data(self) -> dict[str, dict]:
+        train_data = {
+            G_TRAIN_RR_FILE: self.get_rr_graph_with_features(),
+            G_TRAIN_VR_FILE: self.get_v2r_graph_with_features(),
+            G_TRAIN_ASSIGNMENTS_FILE: self.optimisation_solutions,
+            G_TRAIN_INIT_ASSIGNMENTS_FILE: self.current_assignments,
+            G_TRAIN_REQ_FEATURES_FILE: self.get_req_features(),
+            G_TRAIN_VEH_FEATURES_FILE: self.get_veh_features()
+        }
         return train_data
 
-    def get_node_features(self):
-        req_features = self.get_req_features()
-        veh_features = self.get_veh_features()
-        return {**req_features, **veh_features}
-
     def get_veh_features(self):
-        # TODO complete
-        veh_features = {vid: {'node_type': 'vehicle', 'pos': vehicle.pos, 'type': vehicle.veh_type,
-                              'status': vehicle.status.display_name,
-                              'fix_cost': vehicle.daily_fix_cost, 'var_cost': vehicle.distance_cost, 'soc': vehicle.soc}
-                        for
-                        vid, vehicle in self.veh_objs.items()}
+        veh_features = {vid: {G_TRAIN_FEATURE_NODE_TYPE: 'vehicle', G_TRAIN_FEATURE_V_POS: vehicle.pos,
+                              G_TRAIN_FEATURE_TYPE: vehicle.veh_type,
+                              G_TRAIN_FEATURE_STATUS: vehicle.status.display_name, G_TRAIN_FEATURE_SOC: vehicle.soc}
+                        for vid, vehicle in self.veh_objs.items()}
         return veh_features
 
     def get_req_features(self):
-        # TODO complete
         req_features = {
-            # demand
-            # revenue, profit
-            # max_detour_time_factor
-            # max_constant_detour_time
-            rid: {'node_type': 'request', 'o_pos': req.o_pos[0], 'd_pos': req.d_pos[0], 'tw_pe': req.t_pu_earliest,
-                  'tw_pl': req.t_pu_latest, 'tw_dl': req.t_do_latest,
-                  'max_trip_time': req.max_trip_time, } for rid, req in self.active_requests.items()}
+            rid: {G_TRAIN_FEATURE_NODE_TYPE: 'request', G_TRAIN_FEATURE_O_POS: req.o_pos[0],
+                  G_TRAIN_FEATURE_D_POS: req.d_pos[0],
+                  G_TRAIN_FEATURE_TW_PE: req.t_pu_earliest,
+                  G_TRAIN_FEATURE_TW_PL: req.t_pu_latest, G_TRAIN_FEATURE_DIRECT_TT: req.init_direct_tt,
+                  G_TRAIN_FEATURE_DIRECT_TD: req.init_direct_td,
+                  G_TRAIN_FEATURE_MAX_TRIP_TIME: req.max_trip_time, G_TRAIN_FEATURE_STATUS: req.status}
+            for rid, req in self.active_requests.items()}
         return req_features
 
-    def get_travel_time_r2v(self, rid, vid):
+    def get_travel_time_v2r(self, vid, rid):
         v_pos = self.veh_objs[vid].pos
         r_pos = self.active_requests[rid].get_o_stop_info()[0]
-        return {G_TRAIN_FEATURE_TRAVEL_TIME: self.routing_engine.return_travel_costs_1to1(v_pos, r_pos)}
+        return {key: val for key, val in
+                zip([G_TRAIN_FEATURE_TRAVEL_COST, G_TRAIN_FEATURE_TRAVEL_TIME, G_TRAIN_FEATURE_TRAVEL_DIST],
+                    self.routing_engine.return_travel_costs_1to1(v_pos, r_pos))}
 
     def get_travel_time_r2r(self, rid1, rid2):
         """get all travel times between the 6 combinations of rid1 and rid2 origins and destination positions"""
         req1, req2 = self.active_requests[rid1], self.active_requests[rid2]
-        return {G_TRAIN_FEATURE_TRAVEL_TIME: [self.routing_engine.return_travel_costs_1to1(pos1, pos2) for pos1, pos2 in
-                                              self.get_od_pairs(req1, req2)]}
+        return {G_TRAIN_FEATURE_TRAVEL_COST: [{key: val for key, val in
+                                               zip([G_TRAIN_FEATURE_TRAVEL_COST, G_TRAIN_FEATURE_TRAVEL_TIME,
+                                                    G_TRAIN_FEATURE_TRAVEL_DIST],
+                                                   self.routing_engine.return_travel_costs_1to1(pos1, pos2))} for
+                                              pos1, pos2 in
+                                              self.get_od_pool_pairs(req1, req2)]}
 
     @staticmethod
-    def get_od_pairs(req1, req2):
-        return [(req1.o_pos, req1.d_pos), (req2.o_pos, req2.d_pos), (req1.o_pos, req2.d_pos),
-                (req1.d_pos, req2.d_pos), (req1.d_pos, req2.o_pos), (req1.o_pos, req2.o_pos)]
+    def get_od_pool_pairs(req1, req2):
+        return [(req1.d_pos, req2.o_pos), (req1.d_pos, req2.d_pos), (req1.o_pos, req2.o_pos), (req1.o_pos, req2.d_pos)]
 
     def get_rr_graph_with_features(self):
-        return {(rid1, rid2): self.get_travel_time_r2r(rid1, rid2) for rid1, rid2 in self.rr}
+        rr_graph = defaultdict(dict)
+        for rid1, rid2 in self.rr:
+            rr_graph[rid1][rid2] = self.get_travel_time_r2r(rid1, rid2)
+        return rr_graph
 
-    def get_r2v_graph_with_features(self):
-        return {rid: {vid: self.get_travel_time_r2v(rid, vid) for vid in vids} for rid, vids in self.r2v.items()}
+    def get_v2r_graph_with_features(self):
+        return {vid: {rid: self.get_travel_time_v2r(vid, rid) for rid in rids} for vid, rids in self.v2r.items()}
 
     @staticmethod
     def write_pickle(path, data: Dict):
