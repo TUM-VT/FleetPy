15acf8c1981909d2a92cd1e064408d9a3b7b629c
hoda_hamdy <hoda.hamdy@tum.de>
2025-02-13 18:32:55 +0100

    first GNN model commit

--- notebooks/GNN_model/GCN_model.py
+++ notebooks/GNN_model/GCN_model.py
@@ -4,19 +4,17 @@ from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
 
 class GCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
+    def __init__(self, hidden_channels, out_channels):
         super().__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.conv1 = GCNConv(-1, hidden_channels, add_self_loops=False)
+        self.conv2 = GCNConv(hidden_channels, out_channels, add_self_loops=False)
 
-    def forward(self, x: Tensor, edge_index: Tensor, edge_label_index:Tensor) -> Tensor:
-        # x: Node feature matrix of shape [num_nodes, in_channels]
-        # edge_index: Graph connectivity matrix of shape [2, num_edges]
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
 
         # Compute edge embeddings by concatenating node embeddings
-        edge_emb = x[edge_label_index[0]] * x[edge_label_index[1]]  # Element-wise product
+        edge_emb = x[edge_index[0]] * x[edge_index[1]]  # Element-wise product
         return torch.sigmoid(edge_emb.sum(dim=1))  # Binary classification output
