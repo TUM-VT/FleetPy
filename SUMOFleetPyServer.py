862d262893689d00926b1217f788e7fcefed6860
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2025-10-02 10:33:52 +0200

    Update SUMOFleetPyServer.py

--- SUMOFleetPyServer.py
+++ SUMOFleetPyServer.py
@@ -363,12 +363,7 @@ class SUMOFleetPyServer():
             arrivedVehicles_internal = self._update_routes_and_add_vehicles(sim_time)
             # 3) sumo time step
             LOG.info(f"---- Traci Step ----- {sim_time}")
-            for veh_id in traci.vehicle.getTeleportingIDList():
-                #LOG.info("Teleporting Vehicle:", veh_id, "Route: ", traci.vehicle.getRoute(veh_id), "at: ",traci.vehicle.getRoadID(veh_id), "pos: ", traci.vehicle.getPosition(veh_id))
-                if veh_id.startswith("fp_"):
-                    veh_id_fp = self._sumo_v_id_to_fleetpy_v_id(veh_id)
-                    print(f"Teleporting SUMO Veh {veh_id} --> FleetPy Veh {veh_id_fp}")
-                    print(vehicle_to_position_dict.get(veh_id_fp, "not in dict"))
+
             try:
                 traci.simulationStep()
             except Exception as e:
@@ -379,6 +374,8 @@ class SUMOFleetPyServer():
                 print("Average Speed of Vehicles in Simulation:", np.mean(veh_speeds))
                 for veh_id in traci.vehicle.getTeleportingIDList():
                     print("Teleporting Vehicle:", veh_id, traci.vehicle.getRoute(veh_id), "at: ",traci.vehicle.getRoadID(veh_id))
+                    veh_id_fp = self._sumo_v_id_to_fleetpy_v_id(veh_id)
+                    print(vehicle_to_position_dict.get(veh_id_fp, "not in dict"))
                 raise(e)
            # if sim_time == 21680:
                # if "fp_0_356" in traci.vehicle.getIDList() or "fp_0_356" in traci.vehicle.getTeleportingIDList() or "fp_0_356" in traci.simulation.getPendingVehicles():
