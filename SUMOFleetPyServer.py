16f314428c73491caa2ee94e5c140939ee2d0e63
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2025-10-02 17:06:26 +0200

    Update SUMOFleetPyServer.py

--- SUMOFleetPyServer.py
+++ SUMOFleetPyServer.py
@@ -368,15 +368,13 @@ class SUMOFleetPyServer():
             # 3) sumo time step
             LOG.info(f"---- Traci Step ----- {sim_time}")
             if sim_time == 21683:
-                    print("Crash at simtime:", traci.simulation.getTime(),
+                    LOG.info("Removal at simtime:", traci.simulation.getTime(),
                     "Vehicles in Simulation:", len(traci.vehicle.getIDList()),
                     "Vehicles in Teleportation:", len(traci.vehicle.getTeleportingIDList()),
                     "Pending Vehicles:", len(traci.simulation.getPendingVehicles()),
                     f"Vehicles Starting Teleportation: {traci.simulation.getStartingTeleportIDList()}")
-                    breakpoint()
                     traci.vehicle.remove("fp_0_356")
                     arrivedVehicles_internal.update({'fp_0_356': 1740})
-
             try:
                 traci.simulationStep()
             except Exception as e:
