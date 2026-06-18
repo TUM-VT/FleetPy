b6357656a3f5555fa946c89f7bb0daaa19911ba1
Roman <roman.engelhardt@tum.de>
2026-06-18 14:04:16 +0200

    Feat: Big merge of chenhao's broker / intermodal framework. detail changes will be added in changelog

--- src/FleetSimulationBase.py
+++ src/FleetSimulationBase.py
@@ -251,21 +251,6 @@ class FleetSimulationBase:
                                               self.network_stat_f)
         # public transportation module
         LOG.info("Initialization of line-based public transportation...")
-        # pt_type = self.scenario_parameters.get(G_PT_TYPE)
-        # self.gtfs_data_dir = self.dir_names.get(G_DIR_PT)
-        # if pt_type is None or self.gtfs_data_dir is None:
-        #     self.pt = None
-        # elif pt_type == "PTMatrixCrowding":
-        #     pt_module = importlib.import_module("src.pt.PtTTMatrixCrowding")
-        #     self.pt = pt_module.PublicTransportTravelTimeMatrixWithCrowding(self.gtfs_data_dir, self.pt_stat_f,
-        #                                                                     self.scenario_parameters,
-        #                                                                     self.routing_engine, self.zones)
-        # elif pt_type == "PtCrowding":
-        #     pt_module = importlib.import_module("src.pt.PtCrowding")
-        #     self.pt = pt_module.PublicTransportWithCrowding(self.gtfs_data_dir, self.pt_stat_f, self.scenario_parameters,
-        #                                                     self.routing_engine, self.zones)
-        # else:
-        #     raise IOError(f"Public transport module {pt_type} not defined for current simulation environment.")
         self.pt_operator: PTControlBase = None
         self._load_pt_operator()
 
